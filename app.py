3641725fa1a18d5462ebe19cfaff884e6a7541f5
Aadith <aadithr894@gmail.com>
2025-09-25 00:37:56 +0530

    first commit

--- app.py
+++ app.py
@@ -2254,7 +2254,8 @@ def reset_password(token):
 
 # ----------------- START APP -----------------
 # ----------------- START APP -----------------
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
