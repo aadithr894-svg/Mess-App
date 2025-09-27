ff7b5b8afd69833549fbb8222e279dfea184e984
Aadith <aadithr894@gmail.com>
2025-09-27 23:15:26 +0530

    first commit

--- app.py
+++ app.py
@@ -1480,7 +1480,7 @@ from decimal import Decimal
 MESS_WINDOWS = {
     'breakfast': (time(7,30), time(10,30)),
     'lunch': (time(12,0), time(14,0)),
-    'dinner': (time(19,0), time(21,0)),
+    'dinner': (time(19,0), time(23,59)),
 }
 
 @app.route('/admin/generate_bills', methods=['GET', 'POST'])
